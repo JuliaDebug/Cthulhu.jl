409a23da2e21882699d5b13109c2a995ec57db18
Keno Fischer <keno@juliacomputing.com>
2019-01-25 02:37:08 -0500

    Intelligently decide what to print in limited screen space
    Function calls are often too large for a single line, messing up the menu.
    That's probably primarily a TerminalMenus bug (and it should be fixed to
    support multi-line display), but even so in extreme cases (*cough* Zygote),
    function calls can be multiple terminal pages. This implements some logic
    to always print function calls as a single line, falling back through the
    following progression:
    - Full argument types
    - Only the head datatype, but not their parameters
    - Only the number of arguments
    - Nothing
    
    E.g.:
    ```
    In `Zygote._forward` select a call to descend into or ↩ to ascend. [q] to quit.
       %5 = invoke Zygote._forward(…,…,…,…)
       %18 = invoke getindex(::Tuple,::Int64)::Union{}
       %33 = invoke getindex(::Tuple,::Int64)::Union{}
       %65 = invoke getindex(::Tuple,::Int64)::Union{}
       %80 = invoke getindex(::Tuple,::Int64)::Union{}
       %270 = invoke getindex(::Tuple,::Int64)::Union{}
    ```

--- src/Cthulhu.jl
+++ src/Cthulhu.jl
@@ -12,16 +12,68 @@ struct Callsite
     rt
 end
 
-function Base.string(c::Callsite)
-    io = IOBuffer()
-    print(io, "%", c.id, " = invoke ", c.f, "(")
-    TT = c.tt.parameters
-    for T in TT
-        print(io, "::", T, ",")
+mutable struct TextWidthLimiter
+    io::IO
+    width::Int
+    limit::Int
+end
+TextWidthLimiter(io::IO, limit) = TextWidthLimiter(io, 0, limit)
+has_space(limiter::TextWidthLimiter, width::Int) = limiter.width + width < limiter.limit - 1
+has_space(limiter::TextWidthLimiter, s) = has_space(limiter, textwidth(string(s)))
+function Base.print(io::TextWidthLimiter, s::String)
+    io.width == io.limit && return 0
+    width = textwidth(s::String)
+    if has_space(io, width)
+        print(io.io, s)
+        io.width += width
+        return
+    else
+        for c in graphemes(s)
+            cwidth = textwidth(c)
+            if has_space(io, cwidth)
+                print(io, c)
+                io.width += cwidth
+            else
+                break
+            end
+        end
+        print(io, '…')
+        io.width += 1
     end
-    !isempty(TT) && seek(io, position(io)-1)
-    print(io, ")", "::", c.rt)
-    String(take!(io))
+end
+
+function Base.show(io::IO, c::Callsite)
+    limit = get(io, :limit, false)
+    cols = limit ? displaysize(io)[2] : typemax(Int)
+    limiter = TextWidthLimiter(io, cols)
+    print(limiter, string("%", c.id, " = invoke "))
+    if !has_space(limiter, c.f)
+        print(limiter, '…')
+        return
+    end
+    print(limiter, string(c.f))
+    pstrings = map(string, c.tt.parameters)
+    headstrings = map(x->string(x.name), c.tt.parameters)
+    print(limiter, "(")
+    # See if we have space to print all the parameters fully
+    if has_space(limiter, sum(textwidth, pstrings) + 3*length(pstrings))
+        print(limiter, join(map(T->string("::", T), pstrings), ","))
+    # Alright, see if we at least have enough space for each head
+    elseif has_space(limiter, sum(textwidth, headstrings) + 6*length(pstrings))
+        print(limiter, join(map(T->string("::", T, "{…}"), headstrings), ","))
+    # Fine, what about just indicating the number of arguments
+    elseif has_space(limiter, 2*(length(c.tt.parameters)))
+        print(limiter, join(map(T->"…", pstrings), ","))
+    else
+        print(limiter, "…")
+    end
+    print(limiter, ")")
+
+    # If we have space for the return type, print it
+    rts = string(c.rt)
+    if has_space(limiter, textwidth(rts)+2)
+        print(limiter, string("::", rts))
+    end
 end
 
 function Callsite(id, mi, rt)
@@ -234,6 +286,13 @@ end
 """
 const descend = descend_code_typed
 
+function show_as_line(el)
+    reduced_displaysize = displaysize(stdout) .- (0, 3)
+    buf = IOBuffer()
+    show(IOContext(buf, :limit=>true, :displasize=>reduced_displaysize), el)
+    String(take!(buf))
+end
+
 function _descend(@nospecialize(F), @nospecialize(TT); iswarn::Bool, kwargs...)
     methods = code_typed(F, TT; kwargs...)
     if isempty(methods)
@@ -250,7 +309,7 @@ function _descend(@nospecialize(F), @nospecialize(TT); iswarn::Bool, kwargs...)
         iswarn ? code_warntype(F, TT) : display(CI=>rt)
         println()
         TerminalMenus.config(cursor = '•', scroll = :wrap)
-        menu = RadioMenu(vcat(map(string, callsites), ["↩ "]))
+        menu = RadioMenu(vcat(map(show_as_line, callsites), ["↩ "]))
         println("In `$F` select a call to descend into or ↩ to ascend. [q] to quit.")
         cid = request(menu)
         if cid == length(callsites) + 1
