3b74f3b1c4ae8cd09c801037d0bab5e50fc155ab
Takafumi Arakaki <aka.tkf@gmail.com>
2019-07-09 15:21:59 -0700

    Add Cthulhu.CONFIG

--- src/codeview.jl
+++ src/codeview.jl
@@ -1,28 +1,57 @@
+Base.@kwdef mutable struct CthulhuConfig
+    enable_highlighter::Bool = false
+    highlighter::Cmd = `pygmentize -l`
+end
+
+highlighter_exists(config::CthulhuConfig) =
+    Sys.which(config.highlighter.exec[1]) !== nothing
+
+@init begin
+    CONFIG.enable_highlighter = highlighter_exists(CONFIG)
+end
+
+"""
+    Cthulhu.CONFIG
+
+# Options
+- `enable_highlighter::Bool`: Use command line `highlighter` to syntax highlight
+  LLVM and native code.  Set to `true` if `highlighter` exists at the import
+  time.
+- `highlighter::Cmd`: A command line program that receives "llvm" or "asm" as
+  an argument and the code as stdin.  Defaults to `$(CthulhuConfig().highlighter)`.
+"""
+const CONFIG = CthulhuConfig()
+
 const asm_syntax = Ref(:att)
 
-function highlight(io, lexer, x)
-    Sys.which("pygmentize") === nothing && return print(io, x)
-    open(pipeline(`pygmentize -l $lexer`; stdout=io, stderr=stderr), "w") do io
+function highlight(io, x, lexer, config::CthulhuConfig)
+    config.enable_highlighter || return print(io, x)
+    if !highlighter_exists(config)
+        @warn "Highlighter command $(config.highlighter.exec[1]) does not exist."
+        return print(io, x)
+    end
+    cmd = `$(config.highlighter) $lexer`
+    open(pipeline(cmd; stdout=io, stderr=stderr), "w") do io
         print(io, x)
     end
 end
 
-function cthulhu_llvm(io::IO, mi, optimize, debuginfo, params)
+function cthulhu_llvm(io::IO, mi, optimize, debuginfo, params, config::CthulhuConfig)
     dump = InteractiveUtils._dump_function_linfo(
         mi, params.world, #=native=# false,
         #=wrapper=# false, #=strip_ir_metadata=# true,
         #=dump_module=# false, #=syntax=# asm_syntax[],
         optimize, debuginfo ? :source : :none)
-    highlight(io, "llvm", dump)
+    highlight(io, dump, "llvm", config)
 end
 
-function cthulhu_native(io::IO, mi, optimize, debuginfo, params)
+function cthulhu_native(io::IO, mi, optimize, debuginfo, params, config::CthulhuConfig)
     dump = InteractiveUtils._dump_function_linfo(
         mi, params.world, #=native=# true,
         #=wrapper=# false, #=strip_ir_metadata=# true,
         #=dump_module=# false, #=syntax=# asm_syntax[],
         optimize, debuginfo ? :source : :none)
-    highlight(io, "asm", dump)
+    highlight(io, dump, "asm", config)
 end
 
 cthulhu_warntype(args...) = cthulhu_warntype(stdout, args...)
