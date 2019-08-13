7e1e32486b3ca53dacb7d7d6896ff2126d29f495
Lyndon White <oxinabox@ucc.asn.au>
2019-08-13 11:01:17 +0100

    Add newline after dump_params

--- src/Cthulhu.jl
+++ src/Cthulhu.jl
@@ -199,6 +199,7 @@ function _descend(mi::MethodInstance; iswarn::Bool, params=current_params(), opt
         elseif toggle === :dump_params
             @info "Dumping inference cache"
             Core.show(map(((i, x),) -> (i, x.result, x.linfo), enumerate(params.cache)))
+            Core.println()
             display_CI = false
         else
             error("Unknown option $toggle")
