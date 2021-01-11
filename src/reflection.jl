373c29c54efba51c55e03346c4440a6b51182e1f
Chris Elrod <elrodc@gmail.com>
2021-01-11 00:25:24 -0500

    Unwrap `QuoteNode` arguments.

--- src/reflection.jl
+++ src/reflection.jl
@@ -122,6 +122,8 @@ function find_callsites(CI::Core.CodeInfo, mi::Core.MethodInstance, slottypes; p
                         elseif isa(a, SlotOrArgument)
                             a = slottypes[_id(a)]
                             return unwrapconst(a)
+                        elseif isa(a, QuoteNode)
+                            return Core.Typeof(a.value)
                         end
                         Core.Typeof(a)
                     end
