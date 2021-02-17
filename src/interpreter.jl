2c5b03db93a263cb3efce3a7e14ec9d85fff9538
Keno Fischer <keno@alumni.harvard.edu>
2021-02-17 14:32:57 -0500

    Minimal switch to AbstractInterpreter (#126)
    Minimal switch to AbstractInterpreter
    
    This is the minimal work required to swtich Cthulhu to AbstractInterpreter.
    There are several things I would like to build on top of this:
        - Read callsites out of statment info rather than reconstructing it
        - Support for Opaque Closures
        - More consistent use of AbstractInterpreter throughout the API
        - Expose inference remarks
    
    I would like to propose that we make a codebase break at Julia 1.6
    for Cthulhu, moving the current master to a branch and requiring
    Julia 1.7 on Cthulhu master. That gives us the rest of the 1.7 cycle
    to update Cthulhu and figure out what the right interfaces between
    Cthulhu and Base are.

---
+++ src/interpreter.jl
@@ -0,0 +1,63 @@
+using Core.Compiler: AbstractInterpreter, NativeInterpreter, InferenceState,
+    OptimizationState, CodeInfo, CodeInstance, InferenceResult
+
+struct InferredSource
+    src::CodeInfo
+    stmt_infos::Vector{Any}
+    rt::Any
+end
+
+mutable struct CthulhuInterpreter <: AbstractInterpreter
+    native::NativeInterpreter
+
+    unopt::Dict{MethodInstance, InferredSource}
+    opt::Dict{MethodInstance, CodeInstance}
+
+    msgs::Dict{MethodInstance, Vector{Pair{Int, String}}}
+end
+
+CthulhuInterpreter() = CthulhuInterpreter(
+    NativeInterpreter(),
+    Dict{MethodInstance, CodeInfo}(),
+    Dict{MethodInstance, CodeInfo}(),
+    Dict{MethodInstance, Vector{Tuple{Int, String}}}()
+)
+
+import Core.Compiler: InferenceParams, OptimizationParams, get_world_counter,
+    get_inference_cache, code_cache,
+    WorldView, lock_mi_inference, unlock_mi_inference, InferenceState
+
+InferenceParams(ei::CthulhuInterpreter) = InferenceParams(ei.native)
+OptimizationParams(ei::CthulhuInterpreter) = OptimizationParams(ei.native)
+get_world_counter(ei::CthulhuInterpreter) = get_world_counter(ei.native)
+get_inference_cache(ei::CthulhuInterpreter) = get_inference_cache(ei.native)
+
+# No need to do any locking since we're not putting our results into the runtime cache
+lock_mi_inference(ei::CthulhuInterpreter, mi::MethodInstance) = nothing
+unlock_mi_inference(ei::CthulhuInterpreter, mi::MethodInstance) = nothing
+
+code_cache(ei::CthulhuInterpreter) = ei.opt
+Core.Compiler.get(a::Dict, b, c) = Base.get(a,b,c)
+Core.Compiler.get(a::WorldView{<:Dict}, b, c) = Base.get(a.cache,b,c)
+Core.Compiler.haskey(a::Dict, b) = Base.haskey(a, b)
+Core.Compiler.haskey(a::WorldView{<:Dict}, b) =
+    Core.Compiler.haskey(a.cache, b)
+Core.Compiler.setindex!(a::Dict, b, c) = setindex!(a, b, c)
+Core.Compiler.may_optimize(ei::CthulhuInterpreter) = true
+Core.Compiler.may_compress(ei::CthulhuInterpreter) = false
+Core.Compiler.may_discard_trees(ei::CthulhuInterpreter) = false
+
+function Core.Compiler.add_remark!(ei::CthulhuInterpreter, sv::InferenceState, msg)
+    push!(get!(ei.msgs, sv.linfo, Tuple{Int, String}[]),
+        sv.currpc => msg)
+end
+
+function Core.Compiler.finish(state::InferenceState, ei::CthulhuInterpreter)
+    r = invoke(Core.Compiler.finish, Tuple{InferenceState, AbstractInterpreter}, state, ei)
+    ei.unopt[state.linfo] = InferredSource(
+        copy(isa(state.src, OptimizationState) ?
+            state.src.src : state.src),
+        copy(state.stmt_info),
+        state.result.result)
+    return r
+end
