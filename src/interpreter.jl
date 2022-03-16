72227cab9a7444197a26ba4de925afcdd2eb67f6
Shuhei Kadowaki <40514306+aviatesk@users.noreply.github.com>
2022-03-16 17:45:11 +0900

    updates with 1.8 backports (#279)

--- src/interpreter.jl
+++ src/interpreter.jl
@@ -49,11 +49,11 @@ Compiler.get_inference_cache(interp::CthulhuInterpreter) = get_inference_cache(i
 # No need to do any locking since we're not putting our results into the runtime cache
 Compiler.lock_mi_inference(interp::CthulhuInterpreter, mi::MethodInstance) = nothing
 Compiler.unlock_mi_inference(interp::CthulhuInterpreter, mi::MethodInstance) = nothing
-@static if VERSION >= v"1.9.0-DEV.120"
+@static if v"1.8-beta2" <= VERSION < v"1.9-" || VERSION >= v"1.9.0-DEV.120"
 Compiler.method_table(interp::CthulhuInterpreter) = method_table(interp.native)
-else # @static if VERSION >= v"1.9.0-DEV.120"
+else # if v"1.8-beta2" <= VERSION < v"1.9-" || VERSION >= v"1.9.0-DEV.120"
 Compiler.method_table(interp::CthulhuInterpreter, sv::InferenceState) = method_table(interp.native, sv)
-end # @static if VERSION >= v"1.9.0-DEV.120"
+end # if v"1.8-beta2" <= VERSION < v"1.9-" || VERSION >= v"1.9.0-DEV.120"
 struct CthulhuCache
     cache::Dict{MethodInstance, CodeInstance}
 end
