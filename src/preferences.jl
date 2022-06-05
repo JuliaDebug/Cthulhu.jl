c2ca1f8a06cc4a219ecdee818251ec74e9279789
C.Brenhin Keller <cbkeller@dartmouth.edu>
2022-06-05 15:23:51 -0400

    Use uuid when reading and setting preferences

--- src/preferences.jl
+++ src/preferences.jl
@@ -30,7 +30,8 @@ function save_config!(config::CthulhuConfig=CONFIG, force=true)
         @warn "Preferences.jl must be loaded in order to save configuration to file."
     else
         set_preferences! = getfield(mod, :set_preferences!)::Function
-        set_preferences!(Cthulhu,
+        cthulhu_id = UUID("f68482b8-f384-11e8-15f7-abe071a5a75f")
+        set_preferences!(cthulhu_id,
             "enable_highlighter" => config.enable_highlighter,
             "highlighter" => config.highlighter.exec,
             "asm_syntax" => String(config.asm_syntax),
@@ -55,18 +56,19 @@ function read_config!(config::CthulhuConfig)
     if mod == nothing
         @info "Preferences.jl not loaded. Using default configuration."
     else
+        cthulhu_id = UUID("f68482b8-f384-11e8-15f7-abe071a5a75f")
         load_preference = getfield(mod, :load_preference)::Function
-        config.enable_highlighter = load_preference(Cthulhu, "enable_highlighter", config.enable_highlighter)
-        config.highlighter = Cmd(load_preference(Cthulhu, "highlighter", config.highlighter))
-        config.asm_syntax = Symbol(load_preference(Cthulhu, "asm_syntax", config.asm_syntax))
-        config.dead_code_elimination = load_preference(Cthulhu, "dead_code_elimination", config.dead_code_elimination)
-        config.pretty_ast = load_preference(Cthulhu, "pretty_ast", config.pretty_ast)
-        config.debuginfo = Symbol(load_preference(Cthulhu, "debuginfo", config.debuginfo))
-        config.optimize = load_preference(Cthulhu, "optimize", config.optimize)
-        config.iswarn = load_preference(Cthulhu, "iswarn", config.iswarn)
-        config.remarks = load_preference(Cthulhu, "remarks", config.remarks)
-        config.with_effects = load_preference(Cthulhu, "with_effects", config.with_effects)
-        config.inline_cost = load_preference(Cthulhu, "inline_cost", config.inline_cost)
-        config.type_annotations = load_preference(Cthulhu, "type_annotations", config.type_annotations)
+        config.enable_highlighter = load_preference(cthulhu_id, "enable_highlighter", config.enable_highlighter)
+        config.highlighter = Cmd(load_preference(cthulhu_id, "highlighter", config.highlighter))
+        config.asm_syntax = Symbol(load_preference(cthulhu_id, "asm_syntax", config.asm_syntax))
+        config.dead_code_elimination = load_preference(cthulhu_id, "dead_code_elimination", config.dead_code_elimination)
+        config.pretty_ast = load_preference(cthulhu_id, "pretty_ast", config.pretty_ast)
+        config.debuginfo = Symbol(load_preference(cthulhu_id, "debuginfo", config.debuginfo))
+        config.optimize = load_preference(cthulhu_id, "optimize", config.optimize)
+        config.iswarn = load_preference(cthulhu_id, "iswarn", config.iswarn)
+        config.remarks = load_preference(cthulhu_id, "remarks", config.remarks)
+        config.with_effects = load_preference(cthulhu_id, "with_effects", config.with_effects)
+        config.inline_cost = load_preference(cthulhu_id, "inline_cost", config.inline_cost)
+        config.type_annotations = load_preference(cthulhu_id, "type_annotations", config.type_annotations)
     end
 end
