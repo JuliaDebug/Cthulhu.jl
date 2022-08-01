a54a7ef3b90d6105a4eaab4ecd39527da33c626e
Shuhei Kadowaki <aviatesk@gmail.com>
2022-08-01 12:40:40 +0900

    improve docstrings

--- src/Cthulhu.jl
+++ src/Cthulhu.jl
@@ -134,8 +134,7 @@ const var"@descend" = var"@descend_code_typed"
 
 Given a function and a tuple-type, interactively explore the output of
 `code_typed` by descending into `invoke` statements. Type enter to select an
-`invoke` to descend into, select ↩  to ascend, and press q or control-c to
-quit.
+`invoke` to descend into, select `↩`  to ascend, and press `q` or `control-c` to quit.
 
 # Usage:
 ```julia
@@ -166,8 +165,7 @@ descend_code_typed(@nospecialize(args...); kwargs...) =
 
 Given a function and a tuple-type, interactively explore the output of
 `code_warntype` by descending into `invoke` statements. Type enter to select an
-`invoke` to descend into, select ↩  to ascend, and press q or control-c to
-quit.
+`invoke` to descend into, select `↩` to ascend, and press `q` or `control-c` to quit.
 
 # Usage:
 ```julia
