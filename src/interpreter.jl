bbb444157ac9ee71e8944b06722cc93edd1f5784
Shuhei Kadowaki <aviatesk@gmail.com>
2022-08-03 11:20:39 +0900

    use `CC` alias name instead of `Core.Compiler`
    Co-Authored-By: Shuhei Kadowaki <aviatesk@gmail.com>

--- src/interpreter.jl
+++ src/interpreter.jl
@@ -1,6 +1,5 @@
-import Core.Compiler: AbstractInterpreter, NativeInterpreter, InferenceState,
-    OptimizationState, CodeInfo, CodeInstance, InferenceResult, WorldRange,
-    IRCode, SSAValue, inlining_policy
+using .CC: AbstractInterpreter, NativeInterpreter, InferenceState, OptimizationState,
+    CodeInfo, CodeInstance, InferenceResult, WorldRange, WorldView, IRCode, SSAValue
 
 struct InferredSource
     src::CodeInfo
@@ -37,52 +36,52 @@ CthulhuInterpreter(interp::AbstractInterpreter=NativeInterpreter()) =
         Dict{MethodInstance, Remarks}()
     )
 
-import Core.Compiler: InferenceParams, OptimizationParams, get_world_counter,
-    get_inference_cache, code_cache, lock_mi_inference, unlock_mi_inference, method_table
-import Core.Compiler: InferenceState, WorldView
+import .CC: InferenceParams, OptimizationParams, get_world_counter,
+    get_inference_cache, code_cache, lock_mi_inference, unlock_mi_inference, method_table,
+    inlining_policy
 using Base: @invoke
 
-Compiler.InferenceParams(interp::CthulhuInterpreter) = InferenceParams(interp.native)
-Compiler.OptimizationParams(interp::CthulhuInterpreter) = OptimizationParams(interp.native)
-Compiler.get_world_counter(interp::CthulhuInterpreter) = get_world_counter(interp.native)
-Compiler.get_inference_cache(interp::CthulhuInterpreter) = get_inference_cache(interp.native)
+CC.InferenceParams(interp::CthulhuInterpreter) = InferenceParams(interp.native)
+CC.OptimizationParams(interp::CthulhuInterpreter) = OptimizationParams(interp.native)
+CC.get_world_counter(interp::CthulhuInterpreter) = get_world_counter(interp.native)
+CC.get_inference_cache(interp::CthulhuInterpreter) = get_inference_cache(interp.native)
 
 # No need to do any locking since we're not putting our results into the runtime cache
-Compiler.lock_mi_inference(interp::CthulhuInterpreter, mi::MethodInstance) = nothing
-Compiler.unlock_mi_inference(interp::CthulhuInterpreter, mi::MethodInstance) = nothing
+CC.lock_mi_inference(interp::CthulhuInterpreter, mi::MethodInstance) = nothing
+CC.unlock_mi_inference(interp::CthulhuInterpreter, mi::MethodInstance) = nothing
 @static if v"1.8-beta2" <= VERSION < v"1.9-" || VERSION >= v"1.9.0-DEV.120"
-Compiler.method_table(interp::CthulhuInterpreter) = method_table(interp.native)
+CC.method_table(interp::CthulhuInterpreter) = method_table(interp.native)
 else # if v"1.8-beta2" <= VERSION < v"1.9-" || VERSION >= v"1.9.0-DEV.120"
-Compiler.method_table(interp::CthulhuInterpreter, sv::InferenceState) = method_table(interp.native, sv)
+CC.method_table(interp::CthulhuInterpreter, sv::InferenceState) = method_table(interp.native, sv)
 end # if v"1.8-beta2" <= VERSION < v"1.9-" || VERSION >= v"1.9.0-DEV.120"
 struct CthulhuCache
     cache::Dict{MethodInstance, CodeInstance}
 end
 
-Compiler.code_cache(interp::CthulhuInterpreter) = WorldView(CthulhuCache(interp.opt), WorldRange(get_world_counter(interp)))
-Compiler.get(wvc::WorldView{CthulhuCache}, mi::MethodInstance, default) = get(wvc.cache.cache, mi, default)
-Compiler.haskey(wvc::WorldView{CthulhuCache}, mi::MethodInstance) = haskey(wvc.cache.cache, mi)
-Compiler.setindex!(wvc::WorldView{CthulhuCache}, ci::CodeInstance, mi::MethodInstance) = setindex!(wvc.cache.cache, ci, mi)
+CC.code_cache(interp::CthulhuInterpreter) = WorldView(CthulhuCache(interp.opt), WorldRange(get_world_counter(interp)))
+CC.get(wvc::WorldView{CthulhuCache}, mi::MethodInstance, default) = get(wvc.cache.cache, mi, default)
+CC.haskey(wvc::WorldView{CthulhuCache}, mi::MethodInstance) = haskey(wvc.cache.cache, mi)
+CC.setindex!(wvc::WorldView{CthulhuCache}, ci::CodeInstance, mi::MethodInstance) = setindex!(wvc.cache.cache, ci, mi)
 
-Compiler.may_optimize(interp::CthulhuInterpreter) = true
-Compiler.may_compress(interp::CthulhuInterpreter) = false
-Compiler.may_discard_trees(interp::CthulhuInterpreter) = false
-Compiler.verbose_stmt_info(interp::CthulhuInterpreter) = true
+CC.may_optimize(interp::CthulhuInterpreter) = true
+CC.may_compress(interp::CthulhuInterpreter) = false
+CC.may_discard_trees(interp::CthulhuInterpreter) = false
+CC.verbose_stmt_info(interp::CthulhuInterpreter) = true
 
-function Compiler.add_remark!(interp::CthulhuInterpreter, sv::InferenceState, msg)
+function CC.add_remark!(interp::CthulhuInterpreter, sv::InferenceState, msg)
     if !haskey(interp.remarks, sv.linfo)
         interp.remarks[sv.linfo] = Remarks()
     end
     push!(interp.remarks[sv.linfo], sv.currpc => msg)
 end
 
-function Compiler.finish(state::InferenceState, interp::CthulhuInterpreter)
-    res = @invoke Compiler.finish(state::InferenceState, interp::AbstractInterpreter)
-    key = Core.Compiler.any(state.result.overridden_by_const) ? state.result : state.linfo
+function CC.finish(state::InferenceState, interp::CthulhuInterpreter)
+    res = @invoke CC.finish(state::InferenceState, interp::AbstractInterpreter)
+    key = CC.any(state.result.overridden_by_const) ? state.result : state.linfo
     interp.unopt[key] = InferredSource(
         copy(state.src),
         copy(state.stmt_info),
-        isdefined(Core.Compiler, :Effects) ? state.ipo_effects : nothing,
+        isdefined(CC, :Effects) ? state.ipo_effects : nothing,
         state.result.result)
     return res
 end
@@ -93,48 +92,48 @@ function create_cthulhu_source(@nospecialize(x), effects::Effects)
     return OptimizedSource(ir, x.src, x.src.inlineable, effects)
 end
 
-@static if hasmethod(Compiler.transform_result_for_cache,
+@static if hasmethod(CC.transform_result_for_cache,
     (AbstractInterpreter, MethodInstance, WorldRange, Any, Effects))
-    function Compiler.transform_result_for_cache(interp::CthulhuInterpreter,
+    function CC.transform_result_for_cache(interp::CthulhuInterpreter,
         linfo::MethodInstance, valid_worlds::WorldRange, @nospecialize(inferred_result),
-        ipo_effects::Core.Compiler.Effects)
+        ipo_effects::CC.Effects)
         return create_cthulhu_source(inferred_result, ipo_effects)
     end
 else
-    function Compiler.transform_result_for_cache(interp::CthulhuInterpreter,
+    function CC.transform_result_for_cache(interp::CthulhuInterpreter,
         linfo::MethodInstance, valid_worlds::WorldRange, @nospecialize(inferred_result))
         return create_cthulhu_source(inferred_result, Effects())
     end
 end
 
 # branch on https://github.com/JuliaLang/julia/pull/41328
-@static if isdefined(Compiler, :is_stmt_inline)
-function Compiler.inlining_policy(
+@static if isdefined(CC, :is_stmt_inline)
+function CC.inlining_policy(
     interp::CthulhuInterpreter, @nospecialize(src), stmt_flag::UInt8,
     mi::MethodInstance, argtypes::Vector{Any})
     @assert isa(src, OptimizedSource) || isnothing(src)
     if isa(src, OptimizedSource)
-        if Compiler.is_stmt_inline(stmt_flag) || src.isinlineable
+        if CC.is_stmt_inline(stmt_flag) || src.isinlineable
             return src.ir
         end
     else
         # the default inlining policy may try additional effor to find the source in a local cache
-        return @invoke Compiler.inlining_policy(
+        return @invoke CC.inlining_policy(
             interp::AbstractInterpreter, nothing, stmt_flag::UInt8,
             mi::MethodInstance, argtypes::Vector{Any})
     end
     return nothing
 end
-else # @static if isdefined(Compiler, :is_stmt_inline)
-function Compiler.inlining_policy(interp::CthulhuInterpreter)
+else # @static if isdefined(CC, :is_stmt_inline)
+function CC.inlining_policy(interp::CthulhuInterpreter)
     function (src)
         @assert isa(src, OptimizedSource)
         return src.isinlineable ? src.ir : nothing
     end
 end
-end # @static if isdefined(Compiler, :is_stmt_inline)
+end # @static if isdefined(CC, :is_stmt_inline)
 
-function Compiler.finish!(interp::CthulhuInterpreter, caller::InferenceResult)
+function CC.finish!(interp::CthulhuInterpreter, caller::InferenceResult)
     effects = EFFECTS_ENABLED ? caller.ipo_effects : nothing
     caller.src = create_cthulhu_source(caller.src, effects)
 end
