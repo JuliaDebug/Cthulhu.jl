0f980a262bb427265bd7ac530b0329dbe5e06f63
Zentrik <Zentrik@users.noreply.github.com>
2023-06-14 11:37:35 +0100

    Run Tests on Extension

--- test/runtests.jl
+++ test/runtests.jl
@@ -26,3 +26,32 @@ using Test, PerformanceTestTools
         include("test_AbstractInterpreter.jl")
     end
 end
+
+@testset "runtests.jl VSCodeExt" begin
+    module VSCodeServer end
+    @testset "test_Cthulhu.jl" begin
+        include("test_Cthulhu.jl")
+    end
+
+    @testset "test_codeview.jl" begin
+        include("test_codeview.jl")
+    end
+
+    # TODO enable this test on nightly
+    if false
+        @testset "test_irshow.jl" begin
+            include("test_irshow.jl")
+        end
+    else
+        @info "skipped test_irshow.jl"
+    end
+
+    @testset "test_terminal.jl" begin
+        include("test_terminal.jl")
+    end
+
+    @testset "test_AbstractInterpreter.jl" begin
+        include("test_AbstractInterpreter.jl")
+    end
+end
+
