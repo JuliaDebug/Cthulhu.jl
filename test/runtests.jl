45ef6c3913936735b70846ad6b2165a011e9821a
Shuhei Kadowaki <aviatesk@gmail.com>
2024-03-03 17:19:46 +0900

    disable VSCode test temporarily

--- test/runtests.jl
+++ test/runtests.jl
@@ -30,62 +30,64 @@ using Test, PerformanceTestTools
     end
 end
 
-module VSCodeServer
-    using TypedSyntax
+# TODO enable the VSCode-related tests
 
-    struct InlineDisplay
-        is_repl::Bool
-    end
-    const INLAY_HINTS_ENABLED = Ref(true)
-    const DIAGNOSTICS_ENABLED = Ref(true)
+# module VSCodeServer
+#     using TypedSyntax
 
-    inlay_hints = []
-    diagnostics = []
+#     struct InlineDisplay
+#         is_repl::Bool
+#     end
+#     const INLAY_HINTS_ENABLED = Ref(true)
+#     const DIAGNOSTICS_ENABLED = Ref(true)
 
-    function Base.display(d::InlineDisplay, x)
-        if x isa Dict{String, Vector{TypedSyntax.InlayHint}}
-            push!(inlay_hints, x)
-        elseif eltype(x) == TypedSyntax.Diagnostic
-            push!(diagnostics, x)
-        end
-        return nothing
-    end
+#     inlay_hints = []
+#     diagnostics = []
 
-    function reset_test_containers()
-        empty!(inlay_hints)
-        empty!(diagnostics)
-    end
-end
-module TestVSCodeExt # stops modules defined in test files from overwriting stuff from previous test
-using Test, PerformanceTestTools, ..VSCodeServer
-@testset "runtests.jl VSCodeExt" begin
-    @testset "test_Cthulhu.jl" begin
-        include("test_Cthulhu.jl")
-    end
+#     function Base.display(d::InlineDisplay, x)
+#         if x isa Dict{String, Vector{TypedSyntax.InlayHint}}
+#             push!(inlay_hints, x)
+#         elseif eltype(x) == TypedSyntax.Diagnostic
+#             push!(diagnostics, x)
+#         end
+#         return nothing
+#     end
 
-    @testset "test_codeview.jl" begin
-        include("test_codeview.jl")
-        include("test_codeview_vscode.jl")
-    end
+#     function reset_test_containers()
+#         empty!(inlay_hints)
+#         empty!(diagnostics)
+#     end
+# end
+# module TestVSCodeExt # stops modules defined in test files from overwriting stuff from previous test
+# using Test, PerformanceTestTools, ..VSCodeServer
+# @testset "runtests.jl VSCodeExt" begin
+#     @testset "test_Cthulhu.jl" begin
+#         include("test_Cthulhu.jl")
+#     end
 
-    # TODO enable these tests
-    if false
-        @testset "test_irshow.jl" begin
-            include("test_irshow.jl")
-        end
-    else
-        @info "skipped test_irshow.jl"
-    end
-    if false
-        @testset "test_terminal.jl" begin
-            include("test_terminal.jl")
-        end
-    else
-        @info "skipped test_terminal.jl"
-    end
+#     @testset "test_codeview.jl" begin
+#         include("test_codeview.jl")
+#         include("test_codeview_vscode.jl")
+#     end
 
-    @testset "test_AbstractInterpreter.jl" begin
-        include("test_AbstractInterpreter.jl")
-    end
-end
-end
+#     # TODO enable these tests
+#     if false
+#         @testset "test_irshow.jl" begin
+#             include("test_irshow.jl")
+#         end
+#     else
+#         @info "skipped test_irshow.jl"
+#     end
+#     if false
+#         @testset "test_terminal.jl" begin
+#             include("test_terminal.jl")
+#         end
+#     else
+#         @info "skipped test_terminal.jl"
+#     end
+
+#     @testset "test_AbstractInterpreter.jl" begin
+#         include("test_AbstractInterpreter.jl")
+#     end
+# end
+# end
