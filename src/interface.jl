a5c7412ea0bb898d232dcf23b0839c14a188bf3c
Cédric Belmant <cedric.belmant@juliahub.com>
2025-08-17 11:36:12 +0000

    Remove disabled AbstractCursor interface

--- src/interface.jl
+++ src/interface.jl
@@ -60,23 +60,6 @@ function get_override(provider::AbstractProvider, @nospecialize(info))
     return nothing
 end
 
-# """
-#     AbstractCursor
-
-# Required overloads:
-# - `Cthulhu.LookupResult(interp::AbstractInterpreter, curs::AbstractCursor, optimize::Bool)`
-# - `Cthulhu.lookup_constproped(interp::AbstractInterpreter, curs::AbstractCursor, override::InferenceResult, optimize::Bool)`
-# - `Cthulhu.get_ci(curs::AbstractCursor) -> CodeInstance`
-# - `Cthulhu.update_cursor(curs::AbstractCursor, mi::MethodInstance)`
-# - `Cthulhu.navigate(curs::AbstractCursor, callsite::Callsite) -> AbstractCursor`
-# """
-# abstract type AbstractCursor end
-
-# get_ci(curs::AbstractCursor) = error(lazy"""
-# missing `$AbstractCursor` API:
-# `$(typeof(curs))` is required to implement the `$get_ci(curs::$(typeof(curs))) -> CodeInstance` interface.
-# """)
-
 struct LookupResult
     src::Union{CodeInfo,IRCode,Nothing}
     rt
@@ -94,9 +77,6 @@ struct LookupResult
     end
 end
 
-# LookupResult(provider::AbstractProvider, cursor::AbstractCursor, optimize::Bool) =
-#     LookupResult(provider, get_ci(cursor), optimize)
-
 function LookupResult(provider::AbstractProvider, src, optimize::Bool)
     interp = get_abstract_interpreter(provider)
     interp !== nothing && return LookupResult(provider, interp, src, optimize)
@@ -105,16 +85,3 @@ function LookupResult(provider::AbstractProvider, src, optimize::Bool)
         `$(typeof(provider))` is required to implement the `$LookupResult(provider::$(typeof(provider)), src, optimize::Bool)` interface.
     """)
 end
-
-# navigate(curs::AbstractCursor, callsite::Callsite) = error(lazy"""
-# missing `$AbstractCursor` API:
-# `$(typeof(curs))` is required to implement the `$navigate(curs::$(typeof(curs)), callsite::Callsite) -> AbstractCursor` interface.
-# """)
-
-# struct CthulhuCursor <: AbstractCursor
-#     ci::CodeInstance
-# end
-
-# AbstractCursor(provider::AbstractProvider, ci::CodeInstance) = CthulhuCursor(ci)
-# get_ci(curs::CthulhuCursor) = curs.ci
-# navigate(curs::CthulhuCursor, callsite::Callsite) = CthulhuCursor(get_ci(callsite))
