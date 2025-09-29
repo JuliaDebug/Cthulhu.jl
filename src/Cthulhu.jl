JuliaDebug/Cthulhu.jl

commit ac0a42893210ac032e4e09aa9bd9dc751e7a1b4b

make Cthulhu use Compiler.jl as stdlib optionally when it's loaded (#619)
* make Cthulhu use Compiler.jl as stdlib optionally when it's loaded

Using the Compiler stdlib does make things a bit tricky because we have
to consider Compiler.jl's compatibility not just with the package itself,
but also with the runtime system. In the past, we only had to worry
about the former, so issues didn't come up as often (since using
`Core.Compiler` guarantees that the compiler is compatible with the
runtime system always).

This commit changes Cthulhu back to using `Base.Compiler` until the user
specifically loads the `Compiler.jl` standard library.
This should reduce how often Cthulhu breaks (in normal use).

When an user loads Compiler explicitly, then Cthulhu now load the
CthulhuCompilerExt extension module, and wraps the implementations of
exported APIs.

* temp: don't load Compiler.jl stdlib for now

* rm activation, accept all 0.x.y and 1.x.y versions of Compiler stdlib

* update Project.toml

Project.toml
@@ -1,25 +1,11 @@
+authors = ["Valentin Churavy <v.churavy@gmail.com> and contributors"]
 name = "Cthulhu"
 uuid = "f68482b8-f384-11e8-15f7-abe071a5a75f"
 version = "2.16.4"
-authors = ["Valentin Churavy <v.churavy@gmail.com> and contributors"]
-
-[deps]
-CodeTracking = "da1fd8a2-8d9e-5ec2-8556-3022fb5608a2"
-Compiler = "807dbc54-b67e-4c79-8afb-eafe4df6f2e1"
-FoldingTrees = "1eca21be-9b9b-4ed8-839a-6d8ae26b1781"
-InteractiveUtils = "b77e0a4c-d291-57a0-90e8-8db25a27a240"
-JuliaSyntax = "70703baa-626e-46a2-a12c-08ffd08c73b4"
-PrecompileTools = "aea7be01-6a6a-4083-8856-8a6e6704d82a"
-Preferences = "21216c6a-2e73-6563-6e65-726566657250"
-REPL = "3fa0cd96-eef1-5676-8a61-b3b8758bbffb"
-TypedSyntax = "d265eb64-f81a-44ad-a842-4247ee1503de"
-UUIDs = "cf7118a7-6976-5b1a-9a39-7adc72f591a4"
-Unicode = "4ec0a83e-493e-50e2-b9ac-8f72acf5a8f5"
-WidthLimitedIO = "b8c1c048-cf81-46c6-9da0-18c1d99e41f2"
 
 [compat]
 CodeTracking = "0.5, 1"
-Compiler = "0.0.3"
+Compiler = "0, 1"
 FoldingTrees = "1"
 InteractiveUtils = "1.9"
 JuliaSyntax = "0.4"
@@ -32,7 +18,27 @@ Unicode = "1.9"
 WidthLimitedIO = "1"
 julia = "1.10"
 
+[deps]
+CodeTracking = "da1fd8a2-8d9e-5ec2-8556-3022fb5608a2"
+FoldingTrees = "1eca21be-9b9b-4ed8-839a-6d8ae26b1781"
+InteractiveUtils = "b77e0a4c-d291-57a0-90e8-8db25a27a240"
+JuliaSyntax = "70703baa-626e-46a2-a12c-08ffd08c73b4"
+PrecompileTools = "aea7be01-6a6a-4083-8856-8a6e6704d82a"
+Preferences = "21216c6a-2e73-6563-6e65-726566657250"
+REPL = "3fa0cd96-eef1-5676-8a61-b3b8758bbffb"
+TypedSyntax = "d265eb64-f81a-44ad-a842-4247ee1503de"
+UUIDs = "cf7118a7-6976-5b1a-9a39-7adc72f591a4"
+Unicode = "4ec0a83e-493e-50e2-b9ac-8f72acf5a8f5"
+WidthLimitedIO = "b8c1c048-cf81-46c6-9da0-18c1d99e41f2"
+
+[weakdeps]
+Compiler = "807dbc54-b67e-4c79-8afb-eafe4df6f2e1"
+
+[extensions]
+CthulhuCompilerExt = "Compiler"
+
 [extras]
+CthulhuCompilerExt = "a0401a94-d28a-5d0d-bd4d-a83640b62d95"
 DeepDiffs = "ab62b9b5-e342-54a8-a765-a90f495de1a6"
 PerformanceTestTools = "dc46b164-d16f-48ec-a853-60448fc869fe"
 Random = "9a3f8284-a2c9-5f02-9a11-845980a1fd5c"

ext/CthulhuCompilerExt.jl
@@ -1,0 +1,15 @@
+module CthulhuCompilerExt
+
+@static if VERSION ≥ v"1.12.0-DEV.1581"
+    using Compiler: Compiler as CC
+    using Compiler.IRShow: IRShow
+    using Cthulhu: Cthulhu
+
+    function __init__()
+        Cthulhu.CTHULHU_MODULE[] = @__MODULE__
+    end
+
+    include("../src/CthulhuBase.jl")
+end
+
+end

test/setup.jl
@@ -3,9 +3,9 @@ if isdefined(parentmodule(@__MODULE__), :VSCodeServer)
     using ..VSCodeServer
 end
 
-@static if VERSION ≥ v"1.12.0-DEV.1581"
-InteractiveUtils.@activate Compiler # use the Compiler.jl stdlib for the Base reflections too
-end
+# @static if VERSION ≥ v"1.12.0-DEV.1581"
+# InteractiveUtils.@activate Compiler # use the Compiler.jl stdlib for the Base reflections too
+# end
 
 function cthulhu_info(@nospecialize(f), @nospecialize(tt=());
                       optimize=true, interp=Cthulhu.CC.NativeInterpreter())

test/test_Cthulhu.jl
@@ -1,8 +1,8 @@
 module test_Cthulhu
 
 using Test, Cthulhu, StaticArrays, Random
-const CC = Cthulhu.CC
 using Core: Const
+const CC = Cthulhu.CTHULHU_MODULE[].CC
 
 include("setup.jl")
 include("irutils.jl")

commit daf8abd78c2cc7bd91e9506ccb34718692643af6

Fix stack overflow in descend_code_typed
Fixes a typo introduced in ac0a42893210ac032e4e09aa9bd9dc751e7a1b4b.

Project.toml
@@ -1,7 +1,7 @@
 authors = ["Valentin Churavy <v.churavy@gmail.com> and contributors"]
 name = "Cthulhu"
 uuid = "f68482b8-f384-11e8-15f7-abe071a5a75f"
-version = "2.17.7"
+version = "2.17.8"
 
 [compat]
 CodeTracking = "0.5, 1, 2"

src/Cthulhu.jl
@@ -143,7 +143,7 @@ julia> descend_code_typed() do
 ```
 """
 function descend_code_typed(@nospecialize(args...); kwargs...)
-    CTHULHU_MODULE[].descend_code_typed(args...; kwargs...)
+    CTHULHU_MODULE[].descend_code_typed_impl(args...; kwargs...)
 end
 
 """