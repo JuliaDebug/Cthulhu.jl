7203deada5043fe303c5654e60cc576ca3b885d2
Shuhei Kadowaki <40514306+aviatesk@users.noreply.github.com>
2025-12-06 03:52:42 +0900

    improve `AsyncIO` robustness (#687)
    Co-authored-by: Cédric Belmant <cedric.bel@hotmail.fr>

--- src/testing.jl
+++ src/testing.jl
@@ -109,6 +109,7 @@ function Base.unsafe_read(io::AsyncIO, to::Ptr{UInt8}, nb::UInt)
             splice!(buffer, 1:n)
             written += n
         end
+        istaskdone(io.task) && throw(EOFError())
     end
 end
 
